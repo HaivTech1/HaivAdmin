2ceeda9d58b893d7533a44f4f290a3e048608105
HaivTech1 <haivtech1@gmail.com>
2022-08-11 03:53:29 +0100

    update middleware

--- middleware.js
+++ middleware.js
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 export function middleware(request) {
   const cookie = request.cookies.get('app_accessToken')
 
-  // return !cookie ? NextResponse.redirect(new URL('/login', request.url)) : NextResponse.next()
+  return !cookie ? NextResponse.redirect(new URL('/login', request.url)) : NextResponse.next()
 }
 
 export const config = {
